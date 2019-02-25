aa057d3f7f9a009618f882331d501ebb1a524800
Carlos Roman <carlosromanrivera@hotmail.com>
2019-02-25 15:10:28 -0600

    [CRR] Update prolog examples.

---
+++ examples/Prolog/bubble-sort.pl
@@ -0,0 +1,20 @@
+bubble_sort(List,Sorted):-
+  b_sort(List,[],Sorted).
+
+b_sort([],Acc,Acc).
+
+b_sort([H|T],Acc,Sorted):-
+  bubble(H,T,NT,Max),
+  b_sort(NT,[Max|Acc],Sorted).
+
+/* get max element of the subset */
+
+bubble(X,[],[],X).
+
+bubble(X,[Y|T],[Y|NT],Max):-
+  X>Y,
+  bubble(X,T,NT,Max).
+
+bubble(X,[Y|T],[X|NT],Max):-
+  X=<Y,
+  bubble(Y,T,NT,Max).
