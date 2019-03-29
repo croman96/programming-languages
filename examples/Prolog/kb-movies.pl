4371f15a76b00b250d5230e5eb351c03e5509102
Carlos Roman <carlosromanrivera@hotmail.com>
2019-03-29 11:57:33 -0600

    [CRR] Remove examples.

--- examples/Prolog/kb-movies.pl
+++
@@ -1,6 +0,0 @@
-movie(chuck_norris, texas_ranger).
-movie(chuck_norris, the_way_of_the_dragon).
-movie(jackie_chan, rush_hour).
-movie(jackie_chan, shanghai_noon).
-movie(bruce_lee, the_way_of_the_dragon).
-movie(bruce_lee, fist_of_fury).
--- examples/Prolog/split.pl
+++
@@ -1,6 +0,0 @@
-split([],[],[]).
-
-split([H],[H],[]).
-
-split([H1,H2|T],[H1|T1],[H2|T2]):-
-  split(T,T1,T2).
